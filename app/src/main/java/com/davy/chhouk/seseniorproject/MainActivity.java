1114a1d34cc8d5bb8165720ebb476174bddf4e45
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-04-22 22:39:59 +0700

    add rounded corner dependency

--- app/src/main/java/com/davy/chhouk/seseniorproject/MainActivity.java
+++ app/src/main/java/com/davy/chhouk/seseniorproject/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = findViewById(R.id.container);
