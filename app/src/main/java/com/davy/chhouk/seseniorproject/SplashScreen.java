ac08c3ad4001ee6f6ba2a18be573c53fc6268591
Gaanploo Somboon <gaanploo_somboon@cmu.ac.th>
2018-04-22 20:37:33 +0700

    splash screen image, activity, and java

---
+++ app/src/main/java/com/davy/chhouk/seseniorproject/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.davy.chhouk.seseniorproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+    protected boolean _active = true;
+    protected int _splashTime = 3000; // time to display the splash screen in ms
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {
+
+                    startActivity(new Intent(SplashScreen.this,
+                            MainActivity.class));
+                    finish();
+                }
+            };
+        };
+        splashTread.start();
+    }
+}
