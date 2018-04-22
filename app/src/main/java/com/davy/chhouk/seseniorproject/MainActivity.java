3d4dbd5f31af1e6da3ce05ac85183f4cebdc2e7e
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-04-22 21:42:40 +0700

    Remove action bar from activity _main

--- app/src/main/java/com/davy/chhouk/seseniorproject/MainActivity.java
+++ app/src/main/java/com/davy/chhouk/seseniorproject/MainActivity.java
@@ -30,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = findViewById(R.id.container);
